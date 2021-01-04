91310ba46af919056da9c81f838be5ffc786e481
RudraIsTheBest <76434046+RudraIsTheBest@users.noreply.github.com>
2021-01-04 17:47:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var canvas;
+var music;
+var block1,block2,block3,block4;
+var ball;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+block2 = createSprite(295,580,200,30);
+block2.shapeColor = "orange";
+block3 = createSprite(515,580,200,30);
+block3.shapeColor = "red";
+block4 = createSprite(740,580,220,30);
+block4.shapeColor = "green";
+ball = createSprite(random(20,750),100, 40,40);
+ball.shapeColor = "white"
+ 
+
+ball.velocityX = 5;
+ball.velocityY =5;
+
+}
+
+function draw() {
+    background("black");
+ 
+    edges = createEdgeSprites();
+  
+
+  ball.bounceOff(edges);
+
+  
+
+if(ball.isTouching(block1) && ball.bounceOff(block1)){
+    ball.shapeColor = "blue";
+    music.play();
+}
+
+
+if(ball.isTouching(block2)){
+    ball.shapeColor = "orange";
+    ball.velocityX = 0;
+    ball.velocityY = 0;
+    music.stop();
+}
+
+
+if(ball.isTouching(block3) && ball.bounceOff(block3)){
+    ball.shapeColor = "red";
+}
+
+
+if(ball.isTouching(block4) && ball.bounceOff(block4)){
+    ball.shapeColor = "green";
+}
+
+    drawSprites();
+}
